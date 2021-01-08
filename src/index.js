a7d54e89f4ac056a563d9b724fe89cd87d86d895
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-08 20:41:05 +0100

    2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider, connect } from 'react-redux';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
