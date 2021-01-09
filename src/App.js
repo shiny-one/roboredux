4b42aa39fd916d846813ad182755db5353b5ce7e
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-09 13:49:16 +0100

    3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import Scroll from './Scroll';
+import { setSearchField } from './actions';
 // import {robots} from './robots';
 import './App.css';
 import ErrorBoundry from './ErrorBoundry';
@@ -11,59 +13,68 @@ import ErrorBoundry from './ErrorBoundry';
 // 	searchfield: ''
 // }
 
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
 class App extends Component {
 	constructor(){
 		super()
 		this.state = {
 			robots: [],
-			searchfield: ''
+			// searchfield: ''
 		}
 	}
 	componentDidMount() {
+		console.log(this.props.store)
 		document.title = "RoboRedux"
 		fetch('https://jsonplaceholder.typicode.com/users').then(response => {
 			return response.json();
 		})
 		.then(users => {
-			this.setState({robots:users})
+			this.setState({ robots:users })
 		})
 		// this.setState({ robots: robots});
 	}
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value})
-	}
+	// onSearchChange = (event) => {
+	// 	this.setState({ searchfield: event.target.value})
+	// }
 	render(){
-		const filterRobotsName = this.state.robots.filter(robots => {
-			if (robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()))
+		const { robots } = this.state;
+		const { searchField, onSearchChange} = this.props;
+		const filterRobotsName = robots.filter(robots => {
+			if (robots.name.toLowerCase().includes(searchField.toLowerCase()))
 			{
 				return robots.name;
 			}
 
-			else if (robots.email.toLowerCase().includes(this.state.searchfield.toLowerCase()))
+			else if (robots.email.toLowerCase().includes(searchField.toLowerCase()))
 			{
 				return robots.email;
-			}
-						// ispravno
-			// else if (robots.desc.toLowerCase().includes(this.state.searchfield.toLowerCase()))
-			// {
-			// 	return robots.desc;
-			// }
-
-			//neispravno
-			// else { 
-			// 	return robots; 
-			// }
-			// else if(robots.id.toLowerCase().includes(this.state.searchfield.toLowerCase())){
-			// 	return robots.id;
-			// }
-			// else(robots.email.toLowerCase().includes(this.state.searchfield.toLowerCase()))
- 
-				// robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()),
-
-				// robots.email.toLowerCase().includes(this.state.searchfield.toLowerCase())	
+			}	
 			else{
 				return '';
 			}
+			// if (robots.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
+			// {
+			// 	return robots.name;
+			// }
+
+			// else if (robots.email.toLowerCase().includes(this.state.searchField.toLowerCase()))
+			// {
+			// 	return robots.email;
+			// }	
+			// else{
+			// 	return '';
+			// }
 		})
 		
 		// const filterRobots = this.state.robots.filter(robots => {
@@ -80,7 +91,7 @@ class App extends Component {
 		return(
 			<div className='tc'>	
 				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
+				<SearchBox searchChange={onSearchChange}/>
 				<Scroll>
 					<p1>Search found no robots</p1>
 				</Scroll>
@@ -90,7 +101,7 @@ class App extends Component {
 	return(
 		<div className='tc'>	
 			<h1 className='f1'>RoboFriends</h1>
-			<SearchBox searchChange={this.onSearchChange}/>
+			<SearchBox searchChange={onSearchChange}/>
 			<Scroll>
 				<ErrorBoundry>
 					<CardList robots={filterRobotsName}/>
@@ -105,4 +116,4 @@ class App extends Component {
 
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,6 @@
 import { CHANGE_SEARCH_FIELD } from './constants.js'
 
-export const setSeatchField = (text) => ({
+export const setSearchField = (text) => ({
 	type: 'CHANGE_SEARCH_FIELD',
 	payload: text
 })
